5b4babd1219778b256f6565b94ec990ff4cf2b89
Harika <harikakc1997@gmail.com>
2023-03-15 19:39:29 +0530

    Feature: New component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Expenses from './components/Expenses';
 
 function App() {
+  const expenses = [
+    { date: new Date(2020, 8, 12), title: 'Toilet Paper', amount: '94.12' },
+    { date: new Date(2021, 2, 10), title: 'New TV', amount: '799.49' },
+    { date: new Date(2021, 4, 22), title: 'Car Insurance', amount: '294.67' },
+    { date: new Date(2021, 6, 2), title: 'New Desk', amount: '450' },
+    { date: new Date(2022, 8, 20), title: 'Bike Insurance', amount: '124.12' }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Let's get started!!
+      </h1>
+      <Expenses expense={expenses} />
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ExpenseItem from './ExpenseItem'
+
+export default function Expenses(props) {
+  return (
+    <div className='expenses'>
+      <ExpenseItem title={props.expense[0].title} date={props.expense[0].date} amount={props.expense[0].amount} />
+      <ExpenseItem title={props.expense[1].title} date={props.expense[1].date} amount={props.expense[1].amount} />
+      <ExpenseItem title={props.expense[2].title} date={props.expense[2].date} amount={props.expense[2].amount} />
+      <ExpenseItem title={props.expense[3].title} date={props.expense[3].date} amount={props.expense[3].amount} />
+      <ExpenseItem title={props.expense[4].title} date={props.expense[4].date} amount={props.expense[4].amount} />
+    </div>
+  )
+}
